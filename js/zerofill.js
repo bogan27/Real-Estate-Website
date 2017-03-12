6c0c9a4173bc8d16ffb84863db952460bd21bd74
Brandon <brandon.bogan93@gmail.com>
2017-03-12 16:31:47 -0400

    Added DB Monitoring Page

---
+++ js/zerofill.js
@@ -0,0 +1,10 @@
+angular.module('filters', []).filter('zpad', function() {
+	return function(input, n) {
+		if(input === undefined)
+			input = ""
+		if(input.length >= n)
+			return input
+		var zeros = "0".repeat(n);
+		return (zeros + input).slice(-1 * n)
+	};
+});
